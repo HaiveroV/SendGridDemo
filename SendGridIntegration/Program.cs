62f2860d73289309acde315f9d1c94e15a8f05a8
Dimitar Haiverov <mhaiverov@gmail.com>
2019-03-20 15:39:47 +0200

    Updated some info

--- SendGridIntegration/Program.cs
+++ SendGridIntegration/Program.cs
@@ -13,21 +13,21 @@ namespace SendGridIntegration
         }
         static async Task Execute()
         {
-            var apiKey = "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";
+            var apiKey = "Your SendGrid apiKey";
             var client = new SendGridClient(apiKey);
             var msg = new SendGridMessage()
             {
-                From = new EmailAddress("mhaiverov@gmail.com", "Team Kepler"),
+                From = new EmailAddress("YourEmail@example.com", ""),
                 Subject = "SendGrid Test Email",
                 PlainTextContent = "Don't mind me im passing by",
                 HtmlContent = "<strong>Hello there friend! " +
                 "This is an automated message used to prove " +
                 "that the service is working." +
-                "Обичам те мила моя !!!</strong>"
+                </strong>"
             };
             var recipients = new List<EmailAddress>
             {
-                new EmailAddress("petq.d.s@gmail.com"),
+                new EmailAddress("Desired Email u want to send to"),
                 
             };
                 msg.AddTos(recipients);
